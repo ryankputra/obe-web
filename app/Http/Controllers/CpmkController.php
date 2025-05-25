7b4faba26e21f658854ffd160e7eca6643f5ba7c
dyaldy <aldy.i.p43@gmail.com>
2025-05-25 21:35:20 +0700

    CPMK FILTER DIBENERIN LAGI

--- app/Http/Controllers/CpmkController.php
+++ app/Http/Controllers/CpmkController.php
@@ -16,24 +16,62 @@ class CpmkController extends Controller
         return view('cpmk.create', compact('cpls', 'matakuliahs'));
     }
 
-    public function index()
-{
-    $cpmks = Cpmk::with('cpl')
-        ->join('cpls', 'cpmks.kode_cpl', '=', 'cpls.kode_cpl') // ganti ke kolom yang benar
-        ->orderByRaw("CAST(SUBSTRING(cpls.kode_cpl, 4) AS UNSIGNED) ASC")
-        ->orderByRaw("CAST(SUBSTRING(cpmks.kode_cpmk, 6) AS UNSIGNED) ASC")
-        ->select('cpmks.*')
-        ->get();
+    public function index(Request $request)
+    {
+        // Get distinct values for filter dropdowns
+        $availableCpls = Cpl::select('kode_cpl')->distinct()->orderBy('kode_cpl')->get();
+        $availableMatakuliahs = MataKuliah::select('kode_mk')->distinct()->orderBy('kode_mk')->get();
+        $availablePics = Cpmk::select('pic')->distinct()->orderBy('pic')->pluck('pic');
 
-    $cpls = Cpl::all()->sortBy(function ($item) {
-        return (int) filter_var($item->kode_cpl, FILTER_SANITIZE_NUMBER_INT);
-    });
+        // Build the query with CPL relationship and sorting
+        $query = Cpmk::with('cpl')
+            ->join('cpls', 'cpmks.kode_cpl', '=', 'cpls.kode_cpl');
 
-    $matakuliahs = MataKuliah::all();
+        // Apply filters
+        if ($request->filled('kode_cpmk')) {
+            $query->where('cpmks.kode_cpmk', 'like', '%' . $request->kode_cpmk . '%');
+        }
 
-    return view('cpmk.index', compact('cpmks', 'cpls', 'matakuliahs'));
-}
+        if ($request->filled('kode_cpl')) {
+            $query->where('cpmks.kode_cpl', $request->kode_cpl);
+        }
 
+        if ($request->filled('mata_kuliah')) {
+            $query->where('cpmks.mata_kuliah', $request->mata_kuliah);
+        }
+
+        if ($request->filled('pic')) {
+            $query->where('cpmks.pic', $request->pic);
+        }
+
+        if ($request->filled('bobot')) {
+            $query->where('cpmks.bobot', $request->bobot);
+        }
+
+        // Apply sorting
+        $query->orderByRaw("CAST(SUBSTRING(cpls.kode_cpl, 4) AS UNSIGNED) ASC")
+            ->orderByRaw("CAST(SUBSTRING(cpmks.kode_cpmk, 6) AS UNSIGNED) ASC")
+            ->select('cpmks.*');
+
+        // Get CPLs for modals with proper sorting
+        $cpls = Cpl::all()->sortBy(function ($item) {
+            return (int) filter_var($item->kode_cpl, FILTER_SANITIZE_NUMBER_INT);
+        });
+
+        $matakuliahs = MataKuliah::all();
+
+        // Paginate the results
+        $cpmks = $query->paginate(10);
+
+        return view('cpmk.index', [
+            'cpmks' => $cpmks,
+            'availableCpls' => $availableCpls,
+            'availableMatakuliahs' => $availableMatakuliahs,
+            'availablePics' => $availablePics,
+            'cpls' => $cpls,
+            'matakuliahs' => $matakuliahs,
+        ]);
+    }
 
 
     // app/Http/Controllers/CpmkController.php (Store Method)
